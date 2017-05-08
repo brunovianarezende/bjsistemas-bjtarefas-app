a1edd80c6a230b9ec904ac6b5dffdb13229df645
Bruno Rezende <brunovianarezende@gmail.com>
2017-05-08 16:46:51 -0300

    changed production service URLs'

--- app/src/release/java/nom/bruno/tasksapp/Constants.java
+++ app/src/release/java/nom/bruno/tasksapp/Constants.java
@@ -1,6 +1,6 @@
 package nom.bruno.tasksapp;
 
 public interface Constants {
-    String INTERNAL_SERVICE_URL = "http://btestsrezende.hopto.org:9876/";
-    String EXTERNAL_SERVICE_URL = "http://192.168.25.3:9876/";
+    String INTERNAL_SERVICE_URL = "http://btestsrezende.hopto.org:9876/tasksservice/";
+    String EXTERNAL_SERVICE_URL = "http://192.168.25.3:9876/tasksservice/";
 }
