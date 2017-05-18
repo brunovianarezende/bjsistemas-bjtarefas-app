d5758b4372f12ef1b9ba028da5fa2304e9e72a48
Bruno Rezende <brunovianarezende@gmail.com>
2017-05-18 16:53:31 -0300

    fix sentry dsn values

--- app/src/deviceDebug/java/nom/bruno/tasksapp/Constants.java
+++ app/src/deviceDebug/java/nom/bruno/tasksapp/Constants.java
@@ -3,5 +3,5 @@ package nom.bruno.tasksapp;
 public interface Constants {
     String INTERNAL_SERVICE_URL = "http://192.168.25.37:8000/";
     String EXTERNAL_SERVICE_URL = "http://btestsrezende.hopto.org:8000/";
-    String SENTRY_DSN = "https://dbc1b392adbe47548777200ffe359d59@sentry.io/169110";
+    String SENTRY_DSN = "https://dbc1b392adbe47548777200ffe359d59:855d76e7bc604498ba2f4c2a64f6a03e@sentry.io/169110";
 }
