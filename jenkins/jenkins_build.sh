dd62db605513b93130fb46b76ea46ef836e10c3e
Bruno Rezende <brunovianarezende@gmail.com>
2017-05-22 11:36:53 -0300

    jenkins_build.sh will only call apksigner

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -5,9 +5,6 @@ STOREPASS=$2
 KEYPASS=$3
 BASEPATH=$4
 INPUT=$BASEPATH/app-release-unsigned.apk
-OUTPUTDIR=$BASEPATH/outputdir
-OUTPUT=$OUTPUTDIR/app-release-signed.apk
+OUTPUT=$BASEPATH/app-release-signed.apk
 
-rm -rf $OUTPUTDIR
-mkdir $OUTPUTDIR
-apksigner sign --ks $KEYSTORE --ks-pass $STOREPASS -ks-key-alias tasksapp --key-pass $KEYPASS $OUTPUT
+apksigner sign --ks $KEYSTORE --ks-pass $STOREPASS -ks-key-alias tasksapp --key-pass $KEYPASS --out $OUTPUT $INPUT
