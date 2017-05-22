d9d0816b91966cc1568d47320b8c7ef80201f822
Bruno Rezende <brunovianarezende@gmail.com>
2017-05-22 09:56:17 -0300

    remove the need of an intermediate file

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -6,10 +6,9 @@ KEYPASS=$3
 BASEPATH=$4
 INPUT=$BASEPATH/app-release-unsigned.apk
 OUTPUTDIR=$BASEPATH/outputdir
-ZIPALIGNED=$OUTPUTDIR/app-release-zipaligned.apk
 OUTPUT=$OUTPUTDIR/app-release-signed.apk
 
 rm -rf $OUTPUTDIR
 mkdir $OUTPUTDIR
-zipalign 4 $INPUT $ZIPALIGNED
+zipalign 4 $INPUT $OUTPUT
 apksigner sign --ks $KEYSTORE --ks-pass $STOREPASS -ks-key-alias tasksapp --key-pass $KEYPASS --out $OUTPUT
