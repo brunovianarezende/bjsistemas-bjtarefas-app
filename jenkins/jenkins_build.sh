91deb36dc845b9b7f12cf541b8f63518ce6be8e2
Bruno Rezende <brunovianarezende@gmail.com>
2017-05-22 09:52:41 -0300

    cleaning up some resources before zipalignin and signing

--- jenkins/jenkins_build.sh
+++ jenkins/jenkins_build.sh
@@ -5,9 +5,11 @@ STOREPASS=$2
 KEYPASS=$3
 BASEPATH=$4
 INPUT=$BASEPATH/app-release-unsigned.apk
-ZIPALIGNED=$BASEPATH/app-release-zipaligned.apk
-OUTPUT=$BASEPATH/app-release-signed.apk
+OUTPUTDIR=$BASEPATH/outputdir
+ZIPALIGNED=$OUTPUTDIR/app-release-zipaligned.apk
+OUTPUT=$OUTPUTDIR/app-release-signed.apk
 
-
+rm -rf $OUTPUTDIR
+mkdir $OUTPUTDIR
 zipalign 4 $INPUT $ZIPALIGNED
 apksigner sign --ks $KEYSTORE --ks-pass $STOREPASS -ks-key-alias tasksapp --key-pass $KEYPASS --out $OUTPUT
